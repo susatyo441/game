ed3aaefd060ed481940abda5afb09a4df6927929
susatyo441 <susatyo441@gmail.com>
2023-06-12 17:40:15 +0700

    animasi

--- Assets/Script/NoteManager.cs
+++ Assets/Script/NoteManager.cs
@@ -8,14 +8,14 @@ public class NoteManager : MonoBehaviour
     bool isCollide1;
     bool isCollide2;
     bool isCollide3;
-    [SerializeField] ParticleSystem vfx_whenPressed1;
+/*    [SerializeField] ParticleSystem vfx_whenPressed1;
     [SerializeField] ParticleSystem vfx_whenPressed2;
     [SerializeField] ParticleSystem vfx_whenPressed11;
     [SerializeField] ParticleSystem vfx_whenPressed21;
     [SerializeField] ParticleSystem vfx_whenPressed12;
     [SerializeField] ParticleSystem vfx_whenPressed22;
     [SerializeField] ParticleSystem vfx_whenPressed13;
-    [SerializeField] ParticleSystem vfx_whenPressed23;
+    [SerializeField] ParticleSystem vfx_whenPressed23;*/
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +27,8 @@ public class NoteManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
-            vfx_whenPressed1.Play();
-            vfx_whenPressed2.Play();
+           /* vfx_whenPressed1.Play();
+            vfx_whenPressed2.Play();*/
 
             if (isCollide)
             {
@@ -36,14 +36,14 @@ public class NoteManager : MonoBehaviour
             }
         }else if (!Input.GetKeyDown(KeyCode.D))
         {
-            vfx_whenPressed1.Stop();
-            vfx_whenPressed2.Stop();
+           /* vfx_whenPressed1.Stop();
+            vfx_whenPressed2.Stop();*/
 
         }
         if (Input.GetKeyDown(KeyCode.F))
         {
-            vfx_whenPressed11.Play();
-            vfx_whenPressed21.Play();
+           /* vfx_whenPressed11.Play();
+            vfx_whenPressed21.Play();*/
 
             if (isCollide1)
             {
@@ -52,14 +52,14 @@ public class NoteManager : MonoBehaviour
         }
         else if (!Input.GetKeyDown(KeyCode.F))
         {
-            vfx_whenPressed11.Stop();
-            vfx_whenPressed21.Stop();
+           /* vfx_whenPressed11.Stop();
+            vfx_whenPressed21.Stop();*/
 
         }
         if (Input.GetKeyDown(KeyCode.J))
         {
-            vfx_whenPressed12.Play();
-            vfx_whenPressed22.Play();
+          /*  vfx_whenPressed12.Play();
+            vfx_whenPressed22.Play();*/
 
             if (isCollide2)
             {
@@ -68,14 +68,14 @@ public class NoteManager : MonoBehaviour
         }
         else if (!Input.GetKeyDown(KeyCode.J))
         {
-            vfx_whenPressed12.Stop();
-            vfx_whenPressed22.Stop();
+           /* vfx_whenPressed12.Stop();
+            vfx_whenPressed22.Stop();*/
 
         }
         if (Input.GetKeyDown(KeyCode.K))
         {
-            vfx_whenPressed13.Play();
-            vfx_whenPressed23.Play();
+           /* vfx_whenPressed13.Play();
+            vfx_whenPressed23.Play();*/
 
             if (isCollide3)
             {
@@ -84,8 +84,8 @@ public class NoteManager : MonoBehaviour
         }
         else if (!Input.GetKeyDown(KeyCode.K))
         {
-            vfx_whenPressed13.Stop();
-            vfx_whenPressed23.Stop();
+            /*vfx_whenPressed13.Stop();
+            vfx_whenPressed23.Stop();*/
 
         }
 
