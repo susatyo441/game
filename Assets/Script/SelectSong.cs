df852222b5c8b9627eb2752a9b920df2148fde9c
ilhamfused <irharijanto20@gmail.com>
2023-07-01 23:03:20 +0700

    fix bug select song
    fix bug select song

--- Assets/Script/SelectSong.cs
+++ Assets/Script/SelectSong.cs
@@ -27,50 +27,60 @@ public class SelectSong : MonoBehaviour
     {
         udb.updateSong(1);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
     public void updateSong2()
     {
         udb.updateSong(2);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
     public void updateSong3()
     {
         udb.updateSong(3);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
     public void updateSong4()
     {
         udb.updateSong(4);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
     public void updateSong5()
     {
         udb.updateSong(5);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
     public void updateSong6()
     {
         udb.updateSong(6);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
     public void updateSong7()
     {
         udb.updateSong(7);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
     public void updateSong8()
     {
         udb.updateSong(8);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
     public void updateSong9()
     {
         udb.updateSong(9);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
     public void updateSong10()
     {
         udb.updateSong(10);                  //UPDATE LAGU YANG DIPILIH
         SceneManager.LoadScene("OnGame");
+        FindObjectOfType<AudioManager>().StopAll();
     }
 }
